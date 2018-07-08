11d50475fbc32d98007df08c7e55ce033823b8cb
jtrtj <jt.r@live.com>
2018-07-08 12:42:44 -0600

    add method to category pass test

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,8 @@ class Category < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :job_categories
   has_many :jobs, through: :job_categories
+
+  def self.ordered_by_name
+    order(:name)
+  end
 end
