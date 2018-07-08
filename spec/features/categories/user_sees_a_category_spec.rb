428003315ea77e21f5ed0abc691ee0265691096b
jtrtj <jt.r@live.com>
2018-07-08 12:33:35 -0600

    start category show page test

---
+++ spec/features/categories/user_sees_a_category_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe 'a user' do
+  context 'visiting /categories/show' do
+    it 'should see a single category' do
+      
+    end
+    it 'should see a categories associated jobs' do
+
+    end
+  end
+end
