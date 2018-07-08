96d4f4cf2420904f3d8a7a00751baf0671957af4
jtrtj <jt.r@live.com>
2018-07-08 17:24:46 -0600

    add partial form for contact, add routes for contact

--- spec/features/company_contacts/company_contacts_features_spec.rb
+++ spec/features/company_contacts/company_contacts_features_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 describe 'a user' do
   context 'visiting company/show' do
     it 'sees a company contact form' do
-      taco_bell = Company.new(name: 'Taco Bell')
+      taco_bell = Company.create!(name: 'Taco Bell')
 
       visit company_path(taco_bell)
 
       fill_in 'Name', with: 'Suzan Surandon'
       fill_in 'Role', with: 'Manager'
-      fill_in 'e-mail', with: 'suzan@tbhq.org'
+      fill_in 'Email', with: 'suzan@tbhq.org'
       click_button 'Save'
 
       expect(current_path).to eq(company_path(taco_bell))
