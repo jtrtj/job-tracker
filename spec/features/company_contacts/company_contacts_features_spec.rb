82b0afec1dfcbbc03950ffc85c84d2b8b91ba7a5
jtrtj <jt.r@live.com>
2018-07-08 17:08:29 -0600

    write test for company contact feature

---
+++ spec/features/company_contacts/company_contacts_features_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe 'a user' do
+  context 'visiting company/show' do
+    it 'sees a company contact form' do
+      taco_bell = Company.new(name: 'Taco Bell')
+
+      visit company_path(taco_bell)
+
+      fill_in 'Name', with: 'Suzan Surandon'
+      fill_in 'Role', with: 'Manager'
+      fill_in 'e-mail', with: 'suzan@tbhq.org'
+      click_button 'Save'
+
+      expect(current_path).to eq(company_path(taco_bell))
+      expect(page).to have_content('Suzan Surandon')
+      expect(page).to have_content('Manager')
+      expect(page).to have_content('suzan@tbhq.org')
+    end
+  end
+end
