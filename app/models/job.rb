0eae2b0551aff096c79d17bfac7c62985dc8951f
jtrtj <jt.r@live.com>
2018-07-09 17:36:22 -0600

    add job validates category id

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,5 @@
 class Job < ApplicationRecord
-  validates :title, :level_of_interest, :city, :description, :company_id, presence: true
+  validates :title, :level_of_interest, :city, :description, :company_id, :category_id, presence: true
   belongs_to :company
   belongs_to :category
   has_many :comments
--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -6,6 +6,7 @@ describe Job do
     it {should validate_presence_of(:description)}
     it {should validate_presence_of(:level_of_interest)}
     it {should validate_presence_of(:company_id)}
+    it {should validate_presence_of(:category_id)}
     it {should validate_presence_of(:city)}
   end
 
