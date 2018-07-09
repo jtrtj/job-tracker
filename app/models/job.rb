3e64f9d48e3dae5b50a54fd2f216605fbdcc3c78
jtrtj <jt.r@live.com>
2018-07-09 17:31:38 -0600

    make migration to add category id to jobs

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,7 +1,6 @@
 class Job < ApplicationRecord
   validates :title, :level_of_interest, :city, :description, :company_id, presence: true
   belongs_to :company
-  has_many :job_categories
-  has_many :categories, through: :job_categories
+  belongs_to :category
   has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180708232606) do
+ActiveRecord::Schema.define(version: 20180709232815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,8 @@ ActiveRecord::Schema.define(version: 20180708232606) do
     t.datetime "updated_at", null: false
     t.bigint "company_id"
     t.string "city"
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_jobs_on_category_id"
     t.index ["company_id"], name: "index_jobs_on_company_id"
   end
 
@@ -67,5 +69,6 @@ ActiveRecord::Schema.define(version: 20180708232606) do
   add_foreign_key "contacts", "companies"
   add_foreign_key "job_categories", "categories"
   add_foreign_key "job_categories", "jobs"
+  add_foreign_key "jobs", "categories"
   add_foreign_key "jobs", "companies"
 end
