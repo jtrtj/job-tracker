721cb4413b300a00ae36392ced4ea179aa5dda0b
jtrtj <jt.r@live.com>
2018-07-11 10:12:47 -0600

    fix delete category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-  has_many :jobs
+  has_many :jobs, dependent: :destroy
 
   def self.ordered_by_name
     order(:name)
