53554762590860126e634c4b670c1ac996d4d3ce
rcm32000 <rcm32000@yahoo.com>
2018-07-11 10:36:30 -0600

    Dynamic testing for Dashboard

--- spec/features/dashboard/dashboard_features_spec.rb
+++ spec/features/dashboard/dashboard_features_spec.rb
@@ -5,26 +5,28 @@ describe 'user' do
     it 'sees a count of jobs by level_of_interest' do
       company = Company.create!(name: 'ESPN')
       category = Category.create!(name: 'sdgfd')
-      job_1 = company.jobs.create!(title: 'Developer',
-                                   description: 'sagf',
-                                   level_of_interest: 3,
-                                   city: 'Denver',
-                                   category_id: category.id)
-      job_2 = company.jobs.create!(title: 'CEO',
-                                   description: 'dfg',
-                                   level_of_interest: 3,
-                                   city: 'Denver',
-                                   category_id: category.id)
-      job_3 = company.jobs.create!(title: 'QA Analyst',
-                                   description: 'sdgfsa',
-                                   level_of_interest: 5,
-                                   city: 'New York City',
-                                   category_id: category.id)
+      job1 = company.jobs.create!(title: 'Developer',
+                                  description: 'sagf',
+                                  level_of_interest: 3,
+                                  city: 'Denver',
+                                  category_id: category.id)
+      job2 = company.jobs.create!(title: 'CEO',
+                                  description: 'dfg',
+                                  level_of_interest: 3,
+                                  city: 'Denver',
+                                  category_id: category.id)
+      job3 = company.jobs.create!(title: 'QA Analyst',
+                                  description: 'sdgfsa',
+                                  level_of_interest: 5,
+                                  city: 'New York City',
+                                  category_id: category.id)
+      level3 = Job.number_of_jobs_per_level_of_interest[3]
+      level5 = Job.number_of_jobs_per_level_of_interest[5]
 
       visit dashboard_index_path
 
-      expect(page).to have_content('2 with interest level 3')
-      expect(page).to have_content('1 with interest level 5')
+      expect(page).to have_content("#{level3} with interest level 3")
+      expect(page).to have_content("#{level5} with interest level 5")
     end
 
     it 'sees the top 3 companies ranked by and with average level_of_interest' do
@@ -32,36 +34,37 @@ describe 'user' do
       company2 = Company.create!(name: 'COOL')
       company3 = Company.create!(name: 'Sad')
       category = Category.create!(name: 'sdgfd')
-      job_1 = company1.jobs.create!(title: 'Developer',
+      job1 = company1.jobs.create!(title: 'Developer',
                                    description: 'sagf',
                                    level_of_interest: 1,
                                    city: 'Denver',
                                    category_id: category.id)
-      job_2 = company1.jobs.create!(title: 'CEO',
+      job2 = company1.jobs.create!(title: 'CEO',
                                    description: 'dfg',
                                    level_of_interest: 3,
                                    city: 'Denver',
                                    category_id: category.id)
-      job_3 = company2.jobs.create!(title: 'QA Analyst',
-                                    description: 'sdgfsa',
-                                    level_of_interest: 2,
-                                    city: 'New York City',
-                                    category_id: category.id)
-      job_4 = company2.jobs.create!(title: 'Acctuary',
-                                    description: 'sdgfsa',
-                                    level_of_interest: 4,
-                                    city: 'New York City',
-                                    category_id: category.id)
-      job_5 = company3.jobs.create!(title: 'Acctuary',
-                                    description: 'sdgfsa',
-                                    level_of_interest: 1,
-                                    city: 'New York City',
-                                    category_id: category.id)
-
+      job3 = company2.jobs.create!(title: 'QA Analyst',
+                                   description: 'sdgfsa',
+                                   level_of_interest: 2,
+                                   city: 'New York City',
+                                   category_id: category.id)
+      job4 = company2.jobs.create!(title: 'Acctuary',
+                                   description: 'sdgfsa',
+                                   level_of_interest: 4,
+                                   city: 'New York City',
+                                   category_id: category.id)
+      job5 = company3.jobs.create!(title: 'Acctuary',
+                                   description: 'sdgfsa',
+                                   level_of_interest: 1,
+                                   city: 'New York City',
+                                   category_id: category.id)
+      cool = Company.average_level_of_interest_by_company[0]
+      espn = Company.average_level_of_interest_by_company[1]
       visit dashboard_index_path
 
-      expect(page).to have_content('ESPN has interest level of 2')
-      expect(page).to have_content('COOL has interest level of 3')
+      expect(page).to have_content("#{espn.name} has interest level of 2")
+      expect(page).to have_content("#{cool.name} has interest level of 3")
       expect(company2.name).to appear_before(company1.name)
     end
 
@@ -69,33 +72,35 @@ describe 'user' do
       company1 = Company.create!(name: 'ESPN')
       company2 = Company.create!(name: 'COOL')
       category = Category.create!(name: 'sdgfd')
-      job_1 = company1.jobs.create!(title: 'Developer',
-                                    description: 'sagf',
-                                    level_of_interest: 1,
-                                    city: 'Denver',
-                                    category_id: category.id)
-      job_2 = company1.jobs.create!(title: 'CEO',
-                                    description: 'dfg',
-                                    level_of_interest: 3,
-                                    city: 'Denver',
-                                    category_id: category.id)
-      job_3 = company2.jobs.create!(title: 'QA Analyst',
-                                    description: 'sdgfsa',
-                                    level_of_interest: 2,
-                                    city: 'New York City',
-                                    category_id: category.id)
+      job1 = company1.jobs.create!(title: 'Developer',
+                                   description: 'sagf',
+                                   level_of_interest: 1,
+                                   city: 'Denver',
+                                   category_id: category.id)
+      job2 = company1.jobs.create!(title: 'CEO',
+                                   description: 'dfg',
+                                   level_of_interest: 3,
+                                   city: 'Denver',
+                                   category_id: category.id)
+      job3 = company2.jobs.create!(title: 'QA Analyst',
+                                   description: 'sdgfsa',
+                                   level_of_interest: 2,
+                                   city: 'New York City',
+                                   category_id: category.id)
+      denver = Job.number_of_jobs_by_city.key(0)
+      new_york = Job.number_of_jobs_by_city.key(1)
 
       visit dashboard_index_path
 
-      expect(page).to have_content('2 located in Denver')
-      expect(page).to have_content('1 located in New York City')
+      expect(page).to have_content("2 located in #{denver}")
+      expect(page).to have_content("1 located in #{new_york}")
 
       click_on 'Denver'
-        
+
       expect(current_path).to eq(jobs_path)
-      expect(page).to have_content(job_1.title)
-      expect(page).to have_content(job_2.title)
-      expect(page).to_not have_content(job_3.title)
+      expect(page).to have_content(job1.title)
+      expect(page).to have_content(job2.title)
+      expect(page).to_not have_content(job3.title)
     end
   end
 end
